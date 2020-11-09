e0d78cd8c3d3670f7c479a7746860354452834e8
sihabembuns369 <embunspadangbulan@gmail.com>
2020-11-09 20:49:37 +0700

    first commit

---
+++ barang.js
@@ -0,0 +1,52 @@
+var gbr = "http://localhost/praktikum4/gambar/";
+var brg = [
+	{
+		nama:'Alat Tulis',
+		tahun:'2019',
+		jumlah:'10',
+		foto: gbr + '1.jpg',
+		deskripsi:'Alat tulis sendiri pun bermacam-macam, mulai dari buku tulis, buku gambar, penghapus, pensil, pulpen, penggaris, amplop, map, gunting, kertas folio, kertas HVS, lakban, kertas kado, kertas mika, dan seterusnya.Pastikan Anda memiliki etalase atau tempat penyimpanan yang cukup untuk meletakkan barang-barang tersebut, ya. Di sisi lain, Anda juga perlu melihat tren bisnis yang sedang berlangsung. Misalnya, saat sedang musim masuk sekolah lagi, Anda bisa memperbanyak stok alat tulis atau memberikan promo agar pelanggan tertarik untuk membeli di toko kelontong Anda.'
+	},
+	{
+		nama:'Sembako',
+		tahun:'2018',
+		jumlah:'9',
+		foto: gbr + '2.jpg',
+		deskripsi:'Sembako atau Sembilan Bahan Pokok juga sering menjadi barang yang diincar masyarakat. Yang termasuk ke dalam Sembako menurut Peraturan Menteri Perdagangan No. 27/2017 adalah beras, jagung, kedelai, gula, minyak goreng, bawang merah, daging sapi, daging ayam, dan telur ayam.'
+	},
+	{
+		nama:'Alat Mandi',
+		tahun:'2014',
+		rating:'8',
+		foto: gbr + '3.jpg',
+		deskripsi:'Selanjutnya, barang yang bisa Anda jual di toko kelontong adalah peralatan mandi dan mencuci. Untuk peralatan mandi sendiri mencakup sabun, sampo, kondisioner, sikat gigi, pasta gigi, sabun muka, hingga deodoran. Untuk peralatan mencuci ada sabun cuci piring, sabun cuci baju, pewangi pakaian.'
+	},
+	{
+		nama:'Camilan',
+		tahun:'2016',
+		rating:'9',
+		foto: gbr + '4.jpg',
+		deskripsi:'Nah, kalau yang satu ini pasti sangat disukai anak-anak. Supaya toko kelontong Anda laris manis, cobalah untuk menjual yang manis-manis, misalnya permen, wafer, coklat, biskuit, dan jajanan enak lainnya yang dapat dibeli dengan harga murah. Di sisi lain, Anda juga bisa menjual bumbu masak, tepuk untuk menggoreng, atau mie instan berbagai rasa yang sering diburu masyarakat. Namun, jika Anda menjual jajanan atau makanan ringan, Anda wajib memeriksa tanggal kedaluwarsa. Jangan sampai Anda menjual barang berkualitas buruk kepada pelanggan Anda. Bisa-bisa, toko kelontong Anda kehilangan kepercayaan dari masyarakat dan mengakibatkan toko Anda sepi..'
+	},
+	{
+		nama:'Gas Elpiji',
+		tahun:'2015',
+		rating:'88',
+		foto: gbr + '5.jpg',
+		deskripsi:'Ada dua jenis gas elpiji yang bisa Anda jual, yakni LPG 3 kg dan LPG 12 kg. Apabila Anda hendak memasukkan dua benda ini ke dalam daftar barang toko kelontong Anda, perhatikan dari segi keamanannya juga, ya. Taruh di tempat yang aman dan hindarkan dari jangkauan anak-anak. Pastikan Anda bisa mengoperasikan atau menggunakan gas elpiji tersebut, ya.'
+	},
+	{
+		nama:'Baju Bayi',
+		tahun:'2011',
+		rating:'71',
+		foto: gbr + '6.jpg',
+		deskripsi:'Keperluan bayi sama pentingnya seperti obat-obatan. Yang termasuk ke dalam kategori ini ialah popok, sampo bayi, sabun bayi, hingga minyak telon..'
+	},
+	{
+		nama:'Obat',
+		tahun:'2014',
+		rating:'83',
+		foto: gbr + '7.jpg',
+		deskripsi:'Bisa minyak kayu putih, minyak angin, obat penurun demam, obat batuk, obat sakit kepala, obat influenza, koyo, salep atau bedak gatal, dan sebagainya. Anda juga bisa menjual Kotak P3K bilang ingin. Obat-obatan ini menjadi sangat penting karena tidak ada yang tahu kapan seseorang akan menderita sakit. Nah, toko kelontong Anda pun bisa menyediakan pertolongan pertama untuk mereka yang membutuhkan.'
+	}
+];
