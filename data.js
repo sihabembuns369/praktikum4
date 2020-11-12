3c1602cc02eae7ff71474600a730754ac22d1562
sihabembuns369 <embunspadangbulan@gmail.com>
2020-11-12 20:40:50 +0700

    a

---
+++ data.js
@@ -0,0 +1,52 @@
+var gbr = "http://siwista.com/gbr/";
+var wst = [
+	{
+		urut : '1',
+		nama:'Candi Arimbi',
+		deskripsi:'Candi Rimbi adalah peninggalan agama Hindu dari masa Klasik yang berada di Kabupaten Jombang, Jawa Timur. Candi ini juga sering disebut Cungkup Pulo. Nama Rimbi dikaitkan dengan nama tokoh pewayangan bernama Arimbi, isteri Werkudara atau yang lebih dikenal dengan nama Bima',
+		alamat:'https://www.google.co.id/maps/place/Candi+Arimbi/@-7.6736943,112.3408575,17z/data=!3m1!4b1!4m5!3m4!1s0x2e786928ffffffff:0x7573a1926fb251ad!8m2!3d-7.6736943!4d112.3430462',
+		foto: gbr + '1.jpg'
+	},
+	{
+	    urut : '2',
+		nama:'Kedung Cinet',
+		deskripsi:'Pemandangan ngarai yang meliuk-liuk dengan dinding batuan yang terukir indah ada di Kedung Cinet. Batuan di dinding ngarai berwarna kuning kecokelatan, sesekali berwarna hijau karena lumut. Bentuk batuan pun begitu unik, seolah diukir oleh tangan-tangan seniman. Diantara bebatuan, terselip tumbuhan semak yang membuat batuan semakin indah dan asri.Diantara sisi batuan, terdapat aliran air yang mengalir dengan tenang. Air di sini pun begitu jernih, tampak berwarna biru kehijauan karena jernihnya. Bahkan, dasar sungai pun tampak sangat jelas, berupa batuan kecil dan pasir dasar sungai. Ditambah lagi, beberapa dasar sungai berundak-undak, yang membuat aliran air menurun seperti air terjun.Suara gemuruh air menjadi irama yang menemani wisata di sini. Desiran dedaunan yang tertiup angin pun turut meramaikan suasana. Sesekali, terdengar suara burung dan hewan hutan yang saling bersahutan. Tak heran, karena lokasinya berada di dalam hutan milik Perhutani',
+		alamat:'https://www.google.co.id/maps/place/Kedung+Cinet/@-7.4369103,112.1318869,17z/data=!3m1!4b1!4m5!3m4!1s0x2e783b8db2cdd47f:0x9652c283b06081b0!8m2!3d-7.4369103!4d112.1340756',
+		foto: gbr + '2.jpg'
+	},
+	{
+	    urut : '3',
+		nama:'Kampung Djawi',
+		deskripsi:'Tempat wisata ini memiliki konsep arsitektur rumah jawa kuno dan terinspirasi dari beberapa rumah adat Jawa Timur. Terdapat 4 tipe bangunan rumah yang siap dijadikan penginapan untuk wisatawan',
+		alamat:'https://www.google.co.id/maps/place/Kampoeng+Djawi/@-7.7007291,112.3878596,17z/data=!3m1!4b1!4m8!3m7!1s0x2e78657087d2bee3:0xc55271ab179c6935!5m2!4m1!1i2!8m2!3d-7.7007291!4d112.3900483',
+		foto: gbr + '3.jpg'
+	},
+	{
+	    urut : '4',
+		nama:'Air Terjun Wonosalam',
+		deskripsi:'Air Terjun Tretes yang terletak di Dusun Pengajaran, Desa Galengdowo, Kecamatan Wonosalam, Kabupaten Jombang. Layak dijadikan referensi berwisata. Ketinggianya mencapai 158 meter dan disebut sebagai air terjun tertinggi di Jawa Timur.Secara geografis letak air terjun Tretes berada di wilayah Kabupaten Jombang. Namun, akses jalan terbagi menjadi dua jalur yakni Kabupaten Kediri dan Jombang. Air terjun tretes terletak di ketinggian 1.250 meter di atas permukaan air laut (dpl). Saat ini obyek wisata alam  Air Terjun Tretes dikelola oleh Taman Hutan Raya (Tahura) Raden Soeryo Jawa timur.Untuk dapat mengakses wisata ini, paling cepat  memilih rute Kecamatan Bareng. Dari Pasar Bareng pengunjung bisa melewati Desa Karangan selanjutnya menuju Desa Galengdowo. Sebab, jika melewati rute Kecamatan Wonosalam jalan yang ditempuh akan lebih jauh karena posisi  Air Tretes berada di ujung selatan Kabupaten Jombang',
+		alamat:'https://www.google.co.id/maps/place/Air+Terjun+Tretes+Kembar+Jarak/@-7.7623987,112.4257417,17z/data=!3m1!4b1!4m5!3m4!1s0x2e7864acdcc094ef:0xa26c49ca5aca1b08!8m2!3d-7.7623987!4d112.4279304',
+		foto: gbr + '4.jpg'
+	},
+	{
+	    urut : '5',
+		nama:'Banyumili Wonosalam',
+		deskripsi:'Wisata alam bernama Banyumili ini memiliki wahana yang beragam. Di antaranya wahana perahu, wahana kampung hobbit, wahana permainan anak-anak dan wahana bermain air di sungai kecil yang ada di bagian bawah lokasi wisata.Pengunjung tidak hanya berasal dari dalam Kabupaten Jombang, dari tetangga kota sekitar pun banyak berdatangan.nLokasinya yang masih asri, nyaman dan bersih menjadi keunggulan tersendiri wisata Banyumili. Lokasinya juga bisa dijangkau dengan kendaraan roda dua dan roda empat atau mini bus',
+		alamat:'https://www.google.com/maps/place/P4S+%26+Eco+Wisata+Banyu+Mili/@-7.6945007,112.3841038,15z/data=!4m2!3m1!1s0x0:0xbb668b74a5fd7b73?sa=X&ved=2ahUKEwjGzNfMj9bsAhVj63MBHYBPCuUQ_BIwGHoECBQQBQ',
+		foto: gbr + '5.jpg'
+	},
+	{
+	    urut : '6',
+		nama:'Sumber Biru',
+		deskripsi:'Sumber Biru adalah sebuah wisata alam yang ada di Desa Wonomerto, Kecamatan Wonosalam, Kabupaten Jombang, Jawa Timur. Wisata alam Sumber Biru berada di kawasan gunung Anjasmoro. Dinamakan Sumber Biru karena saat dulu ditemukan ada sumber air di bawah pohon beringin besar',
+		alamat:'https://www.google.com/maps/place/Wana+Wisata+Sumber+Biru/@-7.733028,112.357439,15z/data=!4m5!3m4!1s0x0:0x8660b3ad0c07573c!8m2!3d-7.733028!4d112.357439',
+		foto: gbr + '6.jpg'
+	},
+	{
+	    urut : '7',
+		nama:'Bale Tani',
+		deskripsi:'Bale Tani adalah wisata buatan yang lebih difokuskan sebagai wisata untuk bersantai, bermain, sambil menikmati kealamian area persawahan dengan beragam keindahan tanam-tanaman yang ada di dalamnya. Intinya, tempat wisata ini sangat cocok untuk Teman Traveler yang ingin melepas penat dari rutinitas sehari-hari. Sementara itu, dinamakan Bale Tani karena sebelumnya area wisata ini menjadi pusat pelatihan pertanian untuk para petani desa setempat. Selain itu, lokasi wisata Bale Tani ini pun juga berada di tengah area persawahan warga',
+		alamat:'https://www.google.com/maps/place/Bale+Tani/@-7.6596492,112.3063632,15z/data=!4m2!3m1!1s0x0:0x4cd74a1182018d4b?sa=X&ved=2ahUKEwii5YeCkNbsAhXnILcAHZRcAmwQ_BIwGHoECBMQBQ',
+		foto: gbr + '7.jpg'
+	}
+];
